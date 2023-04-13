8c16dfdc12b93493b41d943828852685a6f30cb5
ShulingK <timotheimbert61@gmail.com>
2023-04-13 15:00:06 +0200

    organization

---
+++ class/fixedObject.py
@@ -0,0 +1,10 @@
+import pygame
+
+class fixedObject: 
+    def __init__(self, scaleX, scaleY, posX, posY):
+        self.posX = posX
+        self.posY = posY
+        self.scaleX = scaleX
+        self.scaleY = scaleY
+
+
