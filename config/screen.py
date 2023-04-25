0a12608c3e68cf3da75edb3038865b1ce75f7945
Naëlle PEINADO <n.titanium.e@gmail.com>
2023-04-25 09:27:18 +0200

    feature : icon <3

--- config/screen.py
+++ config/screen.py
@@ -7,6 +7,8 @@ SCREEN_HEIGHT = 720
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Blobby")
+icon = pygame.image.load("img/logo.png")
+pygame.display.set_icon(icon)
 
 
 # Redessinage des éléments de la fenetre 
