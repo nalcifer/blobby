b6084a638a2d40f88e192d3982b795b4aa677d77
Naëlle <117281037+nalcifer@users.noreply.github.com>
2023-04-27 00:20:36 +0200

    Update hitbox

--- classes/object.py
+++ classes/object.py
@@ -5,19 +5,19 @@ from config.screen import *
 # Class qui va être utiliser par tous les objets fixes de notre scene (consommables, batiments...)
 class Object:
     # Initialisation des variables des Objects
-    def __init__(self, img, posX, posY, collide, good, rectX=0, rectY=0):
+    def __init__(self, img, posX, posY, collide, good, height = 0, width = 0):
         self.img = img
         self.posX = posX
         self.posY = posY
-        if rectX == 0 :
-            rectX = self.posX
-        if rectY == 0 :
-            rectY = self.posY
-        self.rect = self.img.get_rect(x = rectX, y = rectY)  
+        self.height = height
+        self.width = width
+        self.rect = self.img.get_rect(x = self.posX, y = self.posY)  
         self.collide = collide
         self.good = good
 
-    # Fonction qui dessine les objets
+    # Fonction qui dessine les objets avec collisions
     def draw(self):
         screen.blit(self.img, (self.posX, self.posY))
         self.rect = self.img.get_rect(x = self.posX, y = self.posY)
+        pygame.Rect.inflate_ip(self.rect, -self.width, -self.height)
+        #-- pygame.draw.rect(screen, (250,0,0), self.rect, 2)
